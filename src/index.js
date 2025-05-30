b9485070a03af544489cd9eadeecbdfb4a935258
HappinessGR <99736764+HappinessGR@users.noreply.github.com>
2025-05-30 11:51:40 +0200

    added functionality

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 //alert("welcome to the AL agent ")
+
+let inputButton = document.querySelector("#input-form");
+let poemForm = document.querySelector("#peom-genetor");
+
+poemForm.addEventListener("submit", function(event) {
+    event.preventDefault(); 
+    console.log(inputButton.value);
+
+    generatePoem();
+});
+
+function generatePoem() {
+    let poemPara = document.querySelector("#peom"); 
+    poemPara.innerHTML = "We are writing a poem in French."; 
+}
