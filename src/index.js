80b86c3f4ae75fef3adf6fe398671db99afa530a
HappinessGR <99736764+HappinessGR@users.noreply.github.com>
2025-05-30 13:04:36 +0200

    updated

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ function generatePoem(event) {
   let userInput = document.querySelector("#input-form");
   let context =
     "You are a romantic Poem expert and love to write short poems. Your mission is to generate a 4-line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning.";
-  let prompt = `User instructions: Generate a French poem about ${userInput.value}.sign the poem with'SheCode AL'at the end`;
+  let prompt = `User instructions: Generate a French poem about ${userInput.value}.sign the poem with<strong>'SheCode AL'</strong>at the end`;
   let apiKey = "0a744841eo85af43btc634f9048290bb";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${encodeURIComponent(prompt)}&context=${encodeURIComponent(context)}&key=${apiKey}`;
 
