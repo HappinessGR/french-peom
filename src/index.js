9f6c83c10d53e86d647b281bf26606efc34bbeea
HappinessGR <99736764+HappinessGR@users.noreply.github.com>
2025-05-30 12:53:32 +0200

    API intergration

--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
-//alert("welcome to the AL agent ")
+// alert("Welcome to the AI agent");
 
-let inputButton = document.querySelector("#input-form");
-let poemForm = document.querySelector("#peom-genetor");
-
-poemForm.addEventListener("submit", function(event) {
-    event.preventDefault(); 
-    console.log(inputButton.value);
-
-    generatePoem();
-});
-
-function generatePoem() {
-    let poemPara = document.querySelector("#peom"); 
-    poemPara.innerHTML = "We are writing a poem in French."; 
+function displayPoem(response) {
+  new Typewriter("#poem", {
+    strings: [response.data.answer], 
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
 }
+
+function generatePoem(event) {
+  event.preventDefault(); 
+  let userInput = document.querySelector("#input-form");
+  let context =
+    "You are a romantic Poem expert and love to write short poems. Your mission is to generate a 4-line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning.";
+  let prompt = `User instructions: Generate a French poem about ${userInput.value}.sign the poem with'SheCode AL'at the end`;
+  let apiKey = "0a744841eo85af43btc634f9048290bb";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${encodeURIComponent(prompt)}&context=${encodeURIComponent(context)}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayPoem);
+}
+
+let poemForm = document.querySelector("#peom-genetor"); 
+poemForm.addEventListener("submit", generatePoem);
